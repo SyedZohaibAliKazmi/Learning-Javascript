d185c973a7f31ec2cd8c6e3fe3ff1536a66ea74a
syed zohaib ali kazmi <syedzohaibali9900@gmail.com>
2024-02-22 01:50:24 +0500

    Class-1

---
+++ app.js
@@ -0,0 +1,20 @@
+let myname ="Syed Zohaib Ali Kazmi"
+
+console.log(myname)
+console.log(myname)
+console.log(myname)
+console.log(myname)
+console.log(myname)
+console.log(myname)
+
+let age= 100
+
+console.log(age)
+
+console.log("5+3")
+console.log(5+4)
+console.log(5+15)
+
+// Line no 23 Answer 
+
+console.log(age +2)
