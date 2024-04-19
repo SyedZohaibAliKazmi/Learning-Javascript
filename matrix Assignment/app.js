ee4825eb6fb704ac4e82666b403b797d11067681
syed zohaib ali kazmi <syedzohaibali9900@gmail.com>
2024-04-20 01:47:52 +0500

    Assignment

---
+++ matrix Assignment/app.js
@@ -0,0 +1,55 @@
+
+// =============== Array Addition ================
+
+let matrix1 = [
+    [74, 32, 15],
+    [3, 9, 1],
+    [8, 4, 9],
+  ];
+  let matrix2 = [
+    [8, 1, 6],
+    [3, 5, 7],
+    [4, 2, 10],
+  ];
+ 
+//   for (let i = 0; i < 3; i++) {
+//     for (let j = 0; j < 3; j++) {
+//       console.log(matrix1[i][j] + matrix2[i][j]);
+//     }
+//   }
+
+//   ==================== Multiplication ==================
+
+let matrix3 = [
+    [5, 3, 1],
+    [3, 9, 1],
+    [8, 4, 9],
+  ];
+  let matrix4 = [
+    [5, 1, 6],
+    [3, 5, 7],
+    [4, 2, 1],
+  ];
+ 
+//   for (let i = 0; i < 3; i++) {
+//     for (let j = 0; j < 3; j++) {
+//       console.log(matrix3[i][j] * matrix4[i][j]);
+//     }
+//   }
+//   ===================== Square ========================
+
+let matrix5 = [
+    [5, 3, 1],
+    [3, 9, 4],
+    [8, 4, 9],
+  ];
+  let matrix6 = [
+    [2, 3, 6],
+    [4, 3, 7],
+    [3, 6, 5],
+  ];
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      console.log(matrix5[i][j] ** matrix6[i][j]);
+    }
+  }
